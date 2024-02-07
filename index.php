68b00a570bb231af3245f926f7a1336da2439b34
Krisanath581 <krisanath581@gmail.com>
2024-02-07 23:35:38 +0700

    TEST

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deshboard</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+ <section class="bg-light min-vh-100">
+    <nav class="navbar navbar-expand-sm" id="navbar">  
+    <a class="navbar-brand px-2" href="#">
+    <img src="img/logocri.png" alt="LOGO" width="200" height="60">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container-fluid">
+<div class="row mt-3">
+        <div class="col-sm-3 col-md-2">
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                 Home
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">Info</a>
+            <a href="#" class="list-group-item list-group-item-action">About</a>
+            <a href="#" class="list-group-item list-group-item-action">Contact</a>
+        </div>
+    </div>
+        <div class="col">
+        <div class="alert alert-primary" role="alert">
+        ดาวน์โหลด โปรแกรมทั้งหมด      
+        </div>
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Library</li>
+  </ol>
+</nav>
+        <div class="row">
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-4"> <!-- ด้านซ้าย -->
+                            <img src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png" class="w-100 round-circle" alt="">
+                        </div>
+                        <div class="col-sm">
+                            <h3 class="my-4">Krisana Thammanatayakul</h3>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-sm bg-success text-white">
+                            <div class="text-center p-4">
+                                <h2>37</h2>
+                                <p>Age</p>
+                            </div>
+                        </div>
+                        <div class="col-sm bg-danger text-white">
+                        <div class="text-center p-4">
+                                <h2>37</h2>
+                                <p>Age</p>
+                            </div>
+                        </div>
+                        <div class="col-sm bg-warning text-white">
+                        <div class="text-center p-4">
+                                <h2>37</h2>
+                                <p>Age</p>
+                            </div>
+                        </div>
+                        <div class="col-sm bg-info text-white">
+                        <div class="text-center p-4">
+                                <h2>37</h2>
+                                <p>Age</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4"> <!-- ด้านขวา -->
+        <div class="card h-100">
+                <div class="card-body">
+                    <h3 my-4>You goals this month</h3>
+                    <h5 class="mt-3">Writing</h5>
+                    <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar w-75"></div>
+                    </div>
+                    <h5  class="mt-3">Reding</h5>
+                    <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar w-75"></div>
+                    </div>
+                    <h5  class="mt-3">Kistening</h5>
+                    <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar w-75"></div>
+                    </div>
+                    <h5  class="mt-3">Speaking</h5>
+                    <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar w-75"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+
+        </div>
+    </div>
+</div>
+</div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
